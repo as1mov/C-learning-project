92027dcf1a54bb61edbef33721570319f6337464
José Luis Blanco <42966407+joseluisbn@users.noreply.github.com>
2020-11-06 13:05:36 +0100

    Create comentarios.cs

---
+++ Source/comentarios.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Comentarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Esto es un comentario de una línea.
+
+            Console.WriteLine("Esto es una prueba de comentarios de texto");
+
+            /*Esto es un comentario multilínea.
+             * Estos textos quedan fuera del código
+             * al compilarlo. Su uso es para ayudar
+             * al desarrollador.
+             */
+
+            Console.ReadKey();
+        }
+    }
+}
