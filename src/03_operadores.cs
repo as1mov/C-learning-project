c7c6f05c0ca6d265865ee8ff8a74e5265ba723ad
José Luis Blanco <joseluisbn@gmail.com>
2021-07-10 09:06:37 +0200

    Create 03_operadores.cs

---
+++ src/03_operadores.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace operadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1+2);
+            Console.WriteLine(1-2);
+            Console.WriteLine(1*2);
+            Console.WriteLine(1/2);
+        }
+    }
+}
