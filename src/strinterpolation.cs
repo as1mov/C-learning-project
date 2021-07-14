2aa01b12284ac6accaaac8bb40e1bb175a5e7970
José Luis Blanco <joseluisbn@gmail.com>
2021-07-14 19:26:25 +0200

    Create strinterpolation.cs

---
+++ src/strinterpolation.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace stringterpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Esto es una interpolación de cadenas
+
+            String name = "nombre"
+
+            Console.WriteLine($"Hola, {name}. Un placer conocerte.");
+
+            Console.ReadKey();
+        }
+    }
+}
