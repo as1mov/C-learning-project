72fd3e2bb4184383ca6d18654e1c6dd481622f5f
José Luis Blanco <joseluisbn@gmail.com>
2021-07-17 21:51:43 +0200

    Create Constantes.cs

---
+++ src/Constantes.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Constantes
+{
+    class Program
+    {
+        //Declaramos las constantes fuera del método Main
+
+        const double PI = 3.141592;
+
+        static void Main(string[] args)
+		{
+            
+        Console.WriteLine("El valor inmutable de PI es {0}", PI);
+			  Console.ReadKey();
+        }
+    }
+}
