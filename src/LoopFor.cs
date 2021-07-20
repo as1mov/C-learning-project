c4b28b9e55efefe2c2ce2f7c42b282284aac7b87
José Luis Blanco <joseluisbn@gmail.com>
2021-07-20 19:26:40 +0200

    Update and rename for.cs to LoopFor.cs

---
+++ src/LoopFor.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace LoopFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Definición del bucle (variable con valor inicial; condición; iterador o contador)
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine("Ejemplo de bucle for: " + i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
