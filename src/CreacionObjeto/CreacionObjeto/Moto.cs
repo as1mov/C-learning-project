b76d44dfbbef2c7c6b2c94ed85ea2fc4c6b82f9c
José Luis Blanco <joseluisbn@gmail.com>
2021-07-21 18:05:53 +0200

    Add files via upload

---
+++ src/CreacionObjeto/CreacionObjeto/Moto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreacionObjeto
+{
+    class Moto
+    {
+        //Características (variables)
+
+        public string nombre;
+
+        //Métodos o acciones
+        public void arrancar()
+        {
+            Console.WriteLine("prrrrrrrrnnnnnn");
+        }
+    }
+}
---
+++ src/CreacionObjeto/CreacionObjeto/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CreacionObjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creamos una instancia de clase (creamos el objeto desde una clase)
+
+            Moto harley = new Moto();
+
+            //Accedemos a la variable pública de la clase y asignamos un valor
+            harley.nombre = "Harley Davidson";
+
+            //Invocamos a un método de la clase
+            harley.arrancar();
+            Console.WriteLine(harley.nombre + " ha arrancado");
+
+            Console.ReadKey();
+        }
+    }
+}
