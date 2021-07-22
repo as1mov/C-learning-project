b065084f3c1cb58b52404552618766cc0d5861fe
José Luis Blanco <joseluisbn@gmail.com>
2021-07-22 19:23:53 +0200

    Add files via upload

---
+++ src/Constructores/Constructores/Moto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructores
+{
+    class Moto
+    {
+        /*podemos asignar las variables como privadas, ya que se invocarán desde el constructor.
+        No hay necesidad de acceder a ellas desde fuera de la clase*/
+
+        private string nombre;
+        private string color;
+
+        //Constructor
+        public Moto(string nombre, string color)
+        {
+            this.nombre = nombre;
+            this.color = color;
+        }
+
+        //Métodos o acciones
+
+        public void arrancar()
+        {
+            Console.WriteLine("prrrrrrrnnnnnnn");
+        }
+    }
+}
---
+++ src/Constructores/Constructores/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Constructores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creamos instancia de clase
+            Moto honda = new Moto("honda", "verde");
+
+            honda.arrancar();
+
+            Console.ReadKey();
+        }
+    }
+}
