63403d953164e4bdd42585f82613b8df28dc82b8
José Luis Blanco <joseluisbn@gmail.com>
2021-07-31 14:49:30 +0200

    Create ArrayIrregular.cs

---
+++ src/ArrayIrregular.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ArrayIrregular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Al contrario de los array normales, abrimos y cerramos dos corchetes
+            //Esto indica tiene múltiples arrays dentro de uno solo
+
+            int [][] matriz = new int [3][];
+
+            //El corchete donde está el 3 indica el número de arrays que contiene el array
+
+            Console.ReadKey();
+        }
+    }
+}
